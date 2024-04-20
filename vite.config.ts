e65bf116bcfa977c68c929bcd13d4da759675d98
Yuvraj <singhyuvraj0506@gmail.com>
2024-04-20 12:24:19 +0530

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    fs: {
+      strict: false // Serve index.html for all routes
+    }
+  }
+});
